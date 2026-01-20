f5ff98220600f37642ad2e00563409bc6bc4e981
hamkarifaii <hamkarifai49@gmail.com>
2026-01-20 13:20:50 +0700

    membuat hook use logi

---
+++ src/features/auth/login/hooks/useLogin.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import type {
+  LoginPayload,
+  LoginView,
+} from "@/features/auth/login/types/login.types";
+import { loginService } from "@/features/auth/login/services/login.services";
+import type { AxiosError } from "axios";
+
+export const useLogin = () => {
+  const [loading, setLoading] = useState(false);
+  const [view, setView] = useState<LoginView>("form");
+  const [error, setError] = useState<string | null>(null);
+
+  const login = async (payload: LoginPayload) => {
+    setLoading(true);
+    setView("loading");
+    setError(null);
+
+    try {
+      loginService.login(payload);
+      return setView("success");
+    } catch (error: any) {
+      const message =
+        (error as AxiosError<{ message: string }>).response?.data?.message ||
+        "Terjadi kesalahan";
+
+      setError(message);
+      setLoading(false);
+      setView("form");
+    }
+  };
+
+  return {
+    loading,
+    view,
+    error,
+    login,
+  };
+};
