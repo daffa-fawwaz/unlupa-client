cb4c1cc2983fcd5d944d4808ed228949e32d958f
hamkarifaii <hamkarifai49@gmail.com>
2026-01-20 12:57:12 +0700

    membuat login form dan menambahkannya ke loginpage

---
+++ src/pages/auth/LoginPage.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/features/auth/login/components/LoginForm";
+
+export const LoginPage = () => {
+  return (
+    <>
+      <div className="bg-deep-universe min-h-screen text-white relative">
+        <LoginForm />
+      </div>
+    </>
+  );
+};
