9279a6a342316c06a921cd89c355c4bf67c8f098
hamkarifaii <hamkarifai49@gmail.com>
2026-01-20 12:58:58 +0700

    membuat login types

---
+++ src/features/auth/login/types/login.types.tsx
@@ -0,0 +1,12 @@
+import type { LoginUser } from "@/features/auth/domain/user.types";
+
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  status: string;
+  message: string;
+  data: LoginUser;
+}
