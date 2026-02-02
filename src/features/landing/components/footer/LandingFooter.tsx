736e6213f0d2cf266e3d5fb6b0f7e81eb1912fb5
hamkarifaii <hamkarifai49@gmail.com>
2026-02-02 22:42:47 +0700

    mengubah logo di footer

--- src/features/landing/components/footer/LandingFooter.tsx
+++ src/features/landing/components/footer/LandingFooter.tsx
@@ -4,7 +4,7 @@ export const LandingFooter = () => {
       <div className="container mx-auto px-6">
         <div className="flex items-center justify-center gap-2 mb-6 opacity-80">
           <div className="w-6 h-6 bg-purple-600 rounded-lg flex items-center justify-center text-white text-xs font-bold shadow-[0_0_10px_rgba(147,51,234,0.5)]">
-            B
+            U
           </div>
           <span className="font-bold text-white">UNLUPA</span>
         </div>
