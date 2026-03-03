ff525586db0547c4edd3ab5bb4093643a2a158eb
hamkarifaii <hamkarifai49@gmail.com>
2026-03-03 11:45:16 +0700

    mengubah Hafalan Kosong

--- src/components/ui/HafalanKosong.tsx
+++ src/components/ui/HafalanKosong.tsx
@@ -11,9 +11,6 @@ export const HafalanKosong = ({ hafalan }: { hafalan: string }) => {
         {hafalan} ini masih kosong. Mulailah perjalanan menghafalmu dengan
         menambahkan target hafalan baru.
       </p>
-      <button className="px-8 py-3 bg-white/5 border border-white/10 text-white rounded-2xl hover:bg-white/10 hover:border-white/20 hover:scale-105 transition-all font-bold">
-        Mulai Menghafal Sekarang
-      </button>
     </div>
   );
 };
