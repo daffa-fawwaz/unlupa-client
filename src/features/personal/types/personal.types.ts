89e073e19a088d4c66135df380e67d4b81111777
hamkarifaii <hamkarifai49@gmail.com>
2026-03-22 14:46:09 +0700

    membuat types untuk ruang personal

---
+++ src/features/personal/types/personal.types.ts
@@ -0,0 +1,53 @@
+export interface Book {
+  id: string;
+  owner_id: string;
+  title: string;
+  description: string;
+  cover_image: string;
+  status: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface GetBooksResponse {
+  status: number;
+  message: string;
+  data: Book[];
+  timestamp: string;
+  path: string;
+}
+
+export interface CreateBookPayload {
+  title: string;
+  description: string;
+  cover_image: string;
+}
+
+export interface CreateBookResponse {
+  status: number;
+  message: string;
+  data: Book;
+  timestamp: string;
+  path: string;
+}
+
+export interface UpdateBookPayload {
+  title: string;
+  description: string;
+  cover_image: string;
+}
+
+export interface UpdateBookResponse {
+  status: number;
+  message: string;
+  data: Book;
+  timestamp: string;
+  path: string;
+}
+
+export interface DeleteBookResponse {
+  status: number;
+  message: string;
+  timestamp: string;
+  path: string;
+}
